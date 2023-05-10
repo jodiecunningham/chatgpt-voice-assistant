5ba324681bbf3f3d504285875635ddef6eee0f9b
Jodie Cunningham <jodie.cunningham@gmail.com>
2023-05-10 16:57:07 -0500

    Using playsound for this instead of the local cmd

--- chatgpt_voice_assistant/computer_voice_responder.py
+++ chatgpt_voice_assistant/computer_voice_responder.py
@@ -5,7 +5,7 @@ import subprocess
 from chatgpt_voice_assistant.bases.responder import Responder
 from chatgpt_voice_assistant.bases.text_to_speech_client import TextToSpeechClient
 from chatgpt_voice_assistant.exceptions.respond_error import RespondError
-
+from playsound import playsound
 
 class ComputerVoiceResponder(Responder):
     """Responder that responds to the user with the Computer Voice"""
@@ -34,9 +34,10 @@ class ComputerVoiceResponder(Responder):
             self._text_to_speech_client.convert_text_to_audio(
                 text_to_speak, self._audio_filename
             )
-            cmd = ["afplay", "--rate", str(self._speech_rate), self._audio_filename]
-            logging.debug(cmd)
-            subprocess.call(cmd)
+            # cmd = ["afplay", "--rate", str(self._speech_rate), self._audio_filename]
+            # logging.debug(cmd)
+            # subprocess.call(cmd)
+            playsound(self._audio_filename)
         except Exception as e:
             raise RespondError(f"Error running computer voice response: {e}") from e
         finally:
